22e935141c2a786df485307757738c782b613355
Carter Rabasa <carter.rabasa@gmail.com>
2022-04-04 14:24:47 -0700

    cleanup

--- src/http/post-signup/index.js
+++ src/http/post-signup/index.js
@@ -3,6 +3,13 @@ let fetch = require('node-fetch')
 
 exports.handler = arc.http.async(signup)
 
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 async function signup(req) {
   console.log(req.body)
   let { first_name, last_name, email_address, segment_id } = req.body
@@ -12,31 +19,26 @@ async function signup(req) {
     'Authorization': `Basic ${ Buffer.from(process.env.CIO_SITE_ID + ':' + process.env.CIO_API_KEY).toString('base64') }`,
     'Content-Type': 'application/json'
   }
-  let payload, res, url, body
+  let res, url, body
   // set the payload for the create/update API call
   let created_at = Math.floor(Date.now() / 1000)
-  payload = { created_at, first_name, last_name }
+  body = JSON.stringify({ created_at, first_name, last_name })
   // call REST API to update customer
   res = await fetch(`https://track.customer.io/api/v1/customers/${ encodeURIComponent(email_address) }`, {
-    method: 'PUT',
-    headers,
-    body: JSON.stringify(payload),
+    method: 'PUT', headers, body
   })
-  console.log(await res.text())
-
+  //console.log(await res.text())
+  // wait 2 seconds for customer record to persist before trying to add them to the segment
+  await sleep(2000)
   // if a segment_id exists, add this user to the segment
   if (segment_id !== undefined) {
-    payload = {ids: [ email_address ]}
+    body = JSON.stringify({ids: [ email_address ]})
     url = `https://track.customer.io/api/v1/segments/${ segment_id }/add_customers?id_type=email`
-    console.log(url)
-    body = JSON.stringify(payload)
-    console.log(body)
+    //console.log(url)
+    //console.log(body)
     res = await fetch(url, {
-      method: 'POST',
-      headers,
-      body,
+      method: 'POST', headers, body
     })
-    console.log(await res.text())
   }
   // if a form request, redirection to next steps
   if (req.headers['content-type'] !== 'application/json') {
