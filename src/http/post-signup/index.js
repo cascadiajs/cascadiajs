43c03c6aa336ab6e06b7ec748043a534b4c84789
Carter Rabasa <carter.rabasa@gmail.com>
2022-04-07 16:16:03 -0700

    using CIO events instead of segments

--- src/http/post-signup/index.js
+++ src/http/post-signup/index.js
@@ -3,17 +3,8 @@ let fetch = require('node-fetch')
 
 exports.handler = arc.http.async(signup)
 
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
 async function signup(req) {
-  console.log(req.body)
-  let { first_name, last_name, email_address, segment_id } = req.body
-  
+  let { first_name, last_name, email_address, list } = req.body
   // set customer.io REST API headers
   let headers = {
     'Authorization': `Basic ${ Buffer.from(process.env.CIO_SITE_ID + ':' + process.env.CIO_API_KEY).toString('base64') }`,
@@ -27,15 +18,13 @@ async function signup(req) {
   res = await fetch(`https://track.customer.io/api/v1/customers/${ encodeURIComponent(email_address) }`, {
     method: 'PUT', headers, body
   })
-  //console.log(await res.text())
-  // wait 2 seconds for customer record to persist before trying to add them to the segment
-  await sleep(2000)
-  // if a segment_id exists, add this user to the segment
-  if (segment_id !== undefined) {
-    body = JSON.stringify({ids: [ email_address ]})
-    url = `https://track.customer.io/api/v1/segments/${ segment_id }/add_customers?id_type=email`
-    //console.log(url)
-    //console.log(body)
+  // if a list exists, add this user to the segment
+  if (list !== undefined) {
+    let name = "sign-up"
+    let type = "event"
+    let data = { list }
+    body = JSON.stringify({ name, type, data })
+    url = `https://track.customer.io/api/v1/customers/${ encodeURIComponent(email_address) }/events`
     res = await fetch(url, {
       method: 'POST', headers, body
     })
