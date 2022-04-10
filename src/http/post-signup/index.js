865ede511e51460a240bfe7743fb23a7cc4ec45a
Carter Rabasa <carter.rabasa@gmail.com>
2022-04-10 12:38:12 -0700

    fixed bug with API calls

--- src/http/post-signup/index.js
+++ src/http/post-signup/index.js
@@ -4,18 +4,22 @@ let fetch = require('node-fetch')
 exports.handler = arc.http.async(signup)
 
 async function signup(req) {
-  // List of all possible list choices on the signup form.
-  const LISTS = ['list-general', 'list-cascadiajs', 'list-seattlejs', 'list-pacnw']
   let { first_name, last_name, email_address, list, lists } = req.body
-  // build a subscription object that reflects a timestamp (in seconds) for lists chosen, and null for lists omitted
-  let subscriptions = LISTS.reduce(
-    (a, v) => (
-      { ...a, [v]: (lists.indexOf(v) >= 0 ? Math.floor(Date.now() / 1000) : null)
-    }), {})
   // build profile object
-  let created_at = Math.floor(Date.now() / 1000) // set created_at to a Unix timestamp
+  //let created_at = Math.floor(Date.now() / 1000) // set created_at to a Unix timestamp
   let unsubscribed = false // necessary if the person unsubscribed and is now trying to re-subscribe
-  let profile = { ...subscriptions, first_name, last_name, created_at, unsubscribed }
+  let profile = { first_name, last_name, unsubscribed }
+
+  // if the mailing list web form was submitted, add subscriptions to profile object
+  if (lists !== undefined) {
+    // build a subscription object that reflects a timestamp (in seconds) for lists chosen, and null for lists omitted
+    const LISTS = ['list-general', 'list-cascadiajs', 'list-seattlejs', 'list-pacnw'] // List of all possible list choices on the signup form
+    let subscriptions = LISTS.reduce(
+      (a, v) => (
+        { ...a, [v]: (lists.indexOf(v) >= 0 ? Math.floor(Date.now() / 1000) : null)
+      }), {})
+    profile = { ...subscriptions, ...profile }
+  }
   // set customer.io REST API headers
   let headers = {
     'Authorization': `Basic ${ Buffer.from(process.env.CIO_SITE_ID + ':' + process.env.CIO_API_KEY).toString('base64') }`,
@@ -28,7 +32,7 @@ async function signup(req) {
   res = await fetch(`https://track.customer.io/api/v1/customers/${ encodeURIComponent(email_address) }`, {
     method: 'PUT', headers, body
   })
-  // if a list exists, send it as a sign-up event
+  // if a list parameter exists, send it as a sign-up event
   if (list !== undefined) {
     let name = "sign-up"
     let type = "event"
