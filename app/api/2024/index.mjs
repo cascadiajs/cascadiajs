e186e78fe4bbe312d701a8beb721293d9bf6a847
crtr0 <carter.rabasa@gmail.com>
2024-02-27 07:33:25 -0800

    added fetch data from Astra DB

--- app/api/2024/index.mjs
+++ app/api/2024/index.mjs
@@ -1,8 +1,20 @@
-import allTalks from '../../data/talks.json' assert { type: 'json' }
-import allSpeakers from '../../data/speakers.json' assert { type: 'json' }
+import { AstraDB } from '@datastax/astra-db-ts'
+
+const {
+  ASTRA_DB_API_ENDPOINT,
+  ASTRA_DB_APPLICATION_TOKEN
+} = process.env
+
+const db = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_API_ENDPOINT)
 
 export async function get(req) {
-  let speakers = allTalks.filter(t => t.event_id === "cascadiajs-2024").map(t => allSpeakers.find(s => t.speaker_id === s.id))
+  const speakersCollection = await db.collection("speakers")
+  const talksCollection = await db.collection("talks")
+  // find all talks for CascadiaJS 2024
+  const talks = await talksCollection.find({event_id: "405fff2e-6083-4aae-b518-18a08cacc463"}).toArray()
+  // build a list of speakers for these talks
+  const speakers = await Promise.all(talks.map(async t => { return await speakersCollection.findOne({_id: t.speaker_id}) }))
+
   const { path } = req;
   return {
     json: {
