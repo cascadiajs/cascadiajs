af23d7fa6225824a9e32ac0fccd70727bf6afb24
crtr0 <carter.rabasa@gmail.com>
2024-06-08 12:16:22 -0700

    fixed share URLs

--- app/pages/home.mjs
+++ app/pages/home.mjs
@@ -10,8 +10,8 @@ export default function ({ html, state }) {
             <h2>Virtual Ticket</h2>
             <p><img width="500" src="/ticket?image=true&ticketId=${ ticketId }" /></p>
             <p>
-                <a target="_blank" href="https://twitter.com/intent/tweet?text=I%20just%20bought%20a%20ticket%20to%20%23CascadiaJS%202024!%20If%20you%20register%20using%20my%20virtual%20ticket%20link%2C%20you%27ll%20save%2035%25!%0A%0Ahttps%3A%2F%2Fcascadiajs.com%2Ftickets%3FticketId%3D${ ticketId }">Share on Twitter</a>
-                <a target="_blank" href="https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fcascadiajs.com%2Ftickets%3FticketId%3D${ ticketId }">Share on LinkedIn</a>
+                <a target="_blank" href="https://twitter.com/intent/tweet?text=I%20just%20bought%20a%20ticket%20to%20%23CascadiaJS%202024!%20If%20you%20register%20using%20my%20virtual%20ticket%20link%2C%20you%27ll%20save%2035%25!%0A%0Ahttps%3A%2F%2Fcascadiajs.com%2Fticket%3FticketId%3D${ ticketId }">Share on Twitter</a>
+                <a target="_blank" href="https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fcascadiajs.com%2Fticket%3FticketId%3D${ ticketId }">Share on LinkedIn</a>
                 <a target="_blank" href="/ticket?ticketId=${ ticketId }">Direct Link</a>
             </p>
             <h2>Talk Track</h2>
