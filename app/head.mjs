8e64699ceae9e57b0354ff79dcac0c9a51df5a25
crtr0 <carter.rabasa@gmail.com>
2025-07-22 20:15:26 -0700

    added Meta pixel

--- app/head.mjs
+++ app/head.mjs
@@ -23,6 +23,23 @@ export default function Head(state) {
 
         gtag('config', 'G-XBTPEH9RZW');
       </script>
+      <!-- Meta Pixel Code -->
+      <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1431763387943877');
+      fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=1431763387943877&ev=PageView&noscript=1"
+      /></noscript>
+      <!-- End Meta Pixel Code -->
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>${ title }</title>
